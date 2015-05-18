3450763fead4e3a6686b1e7dda8ad0c927eaf0e0
gozonglor <glor@macalester.edu>
2015-05-18 10:25:36 -0500

    updated phone mask + input field widths.
    cleaned out inline css.

--- js/mask.js
+++ js/mask.js
@@ -1,9 +1,9 @@
 jQuery(function($){
-   $("#phone_f").mask("999-999-9999");
-   $("#phone_new").mask("999-999-9999");
+   //$("#phone_f").mask("9999999999");
+   //$("#phone_new").mask("9999999999");
    //$("#email_f").mask("aaaa@aaaaa.aaaaa");
 
    
-   $("#zip_f").mask("99999");   
-   $("#zip_new").mask("99999");
+  //$("#zip_f").mask("99999");   
+   //$("#zip_new").mask("99999");
 });
